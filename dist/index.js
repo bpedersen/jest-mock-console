87b44b45ac82a9c7c50673c6906ab40334aad37b
Brian Pedersen <bpedersen@i3logix.com>
2018-06-22 09:16:10 -0600

    Src and dist folders seem decoupled
    Added build of dist to repo
    Updated Readme to clarity api

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -6,32 +6,38 @@ Object.defineProperty(exports, "__esModule", {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var mockConsole = function mockConsole() {
-  var originalConsole = _extends({}, console);
-  console.log = jest.fn(function (string) {
-    return {
-      show: function show() {
-        return originalConsole.log(string);
-      }
-    };
-  });
-  console.warn = jest.fn(function (string) {
-    return {
-      show: function show() {
-        return originalConsole.warn(string);
-      }
-    };
-  });
-  console.error = jest.fn(function (string) {
-    return {
-      show: function show() {
-        return originalConsole.error(string);
-      }
-    };
-  });
+var defaultKeys = ['log', 'warn', 'error'];
 
+var mockConsole = function mockConsole(mockArg) {
+  var originalConsole = _extends({}, console);
+  // No argument
+  if (!mockArg) {
+    defaultKeys.forEach(function (key) {
+      global.console[key] = jest.fn();
+    });
+  }
+  // Argument is a string
+  else if (typeof mockArg === 'string' || mockArg instanceof String) {
+      global.console[mockArg] = jest.fn();
+    }
+    // Argument is an array
+    else if (Array.isArray(mockArg)) {
+        mockArg.forEach(function (key) {
+          global.console[key] = jest.fn();
+        });
+      }
+      // Argument is an object
+      else {
+          Object.keys(mockArg).forEach(function (key) {
+            global.console[key] = mockArg[key];
+          });
+        }
+  // Return function to restore console
+  var restore = function restore() {
+    return global.console = originalConsole;
+  };
   return function () {
-    console = originalConsole;
+    global.console = originalConsole;
   };
 };
 
--- dist/setupTestFramework.js
+++ dist/setupTestFramework.js
@@ -2,25 +2,22 @@
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _console = _extends({}, console),
-    log = _console.log,
-    warn = _console.warn,
-    error = _console.error;
-
 var originalDescribe = jasmine.getEnv().describe;
 
 jasmine.getEnv().describe = function (description, specDefinitions) {
-  for (var _len = arguments.length, args = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
-    args[_key - 2] = arguments[_key];
+  for (var _len = arguments.length, describeArgs = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    describeArgs[_key - 2] = arguments[_key];
   }
 
+  var $jestMockConsoleOriginal = void 0;
   var injectedSpecDefinition = function injectedSpecDefinition() {
-    afterEach(function () {
-      console.log = log;
-      console.warn = warn;
-      console.error = error;
+    beforeEach(function () {
+      $jestMockConsoleOriginal = _extends({}, console);
     });
-    specDefinitions();
+    afterEach(function () {
+      global.console = $jestMockConsoleOriginal;
+    });
+    specDefinitions.apply(undefined, arguments);
   };
-  return originalDescribe.apply(undefined, [description, injectedSpecDefinition].concat(args));
+  return originalDescribe.apply(undefined, [description, injectedSpecDefinition].concat(describeArgs));
 };
