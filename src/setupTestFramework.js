ee5f0170a709c66759e5600ae760096a002a6999
bpedersen <brianhpedersen@hotmail.com>
2021-09-29 20:56:37 -0600

    Fix for jest@27 when running without jasmine

--- src/setupTestFramework.js
+++ src/setupTestFramework.js
@@ -1,16 +1,42 @@
-const originalDescribe = jasmine.getEnv().describe;
+const originalDescribe = describe;
 
-
-jasmine.getEnv().describe = (description, specDefinitions, ...describeArgs) => {
-  let $jestMockConsoleOriginal;
-  const injectedSpecDefinition = (...specArgs) => {
-    beforeEach(() => {
-       $jestMockConsoleOriginal = {...console};
-    });
-    afterEach(() => {
-      global.console = $jestMockConsoleOriginal;
-    });
-    specDefinitions(...specArgs);
+// Check to see if version before 27 where jasmine is default
+const jestVersion = require("jest/package.json").version;
+const [majorVersion] = jestVersion.split(".");
+if (majorVersion < 27) {
+  jasmine.getEnv().describe = (
+    description,
+    specDefinitions,
+    ...describeArgs
+  ) => {
+    let $jestMockConsoleOriginal;
+    const injectedSpecDefinition = (...specArgs) => {
+      beforeEach(() => {
+        $jestMockConsoleOriginal = { ...console };
+      });
+      afterEach(() => {
+        global.console = $jestMockConsoleOriginal;
+      });
+      specDefinitions(...specArgs);
+    };
+    return originalDescribe(
+      description,
+      injectedSpecDefinition,
+      ...describeArgs
+    );
   };
-  return originalDescribe(description, injectedSpecDefinition, ...describeArgs);
+} else {
+  describe = (description, specDefinitions) => {
+    let $jestMockConsoleOriginal;
+    const injectedSpecDefinition = (...specArgs) => {
+      beforeEach(() => {
+        $jestMockConsoleOriginal = { ...console };
+      });
+      afterEach(() => {
+        global.console = $jestMockConsoleOriginal;
+      });
+      return specDefinitions(...specArgs);
+    };
+    return originalDescribe(description, injectedSpecDefinition);
+  };
 }
