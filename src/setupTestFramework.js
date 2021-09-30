d86d0395a5c0b9bf86008dec8ed3996e0c44e81c
bpedersen <brianhpedersen@hotmail.com>
2021-09-30 07:25:05 -0600

    Fixed version for jest26

--- src/setupTestFramework.js
+++ src/setupTestFramework.js
@@ -1,9 +1,9 @@
-const originalDescribe = describe;
-
 // Check to see if version before 27 where jasmine is default
 const jestVersion = require("jest/package.json").version;
 const [majorVersion] = jestVersion.split(".");
 if (majorVersion < 27) {
+  const originalDescribe = jasmine.getEnv().describe;
+
   jasmine.getEnv().describe = (
     description,
     specDefinitions,
@@ -26,6 +26,8 @@ if (majorVersion < 27) {
     );
   };
 } else {
+  const originalDescribe = describe;
+
   describe = (description, specDefinitions) => {
     let $jestMockConsoleOriginal;
     const injectedSpecDefinition = (...specArgs) => {
