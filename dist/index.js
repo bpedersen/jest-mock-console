bcc3fa66fee29cf06bc3f8720703cf30a420b542
bpedersen <brianhpedersen@hotmail.com>
2021-09-29 21:13:00 -0600

    Removed workaround for jest@27

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
@@ -23,23 +23,19 @@ var mockConsole = function mockConsole(mockArg) {
     });
   } // Argument is a string
   else if (typeof mockArg === 'string' || mockArg instanceof String) {
-      global.console[mockArg] = jest.fn();
-    } // Argument is an array
-    else if (Array.isArray(mockArg)) {
-        mockArg.forEach(function (key) {
-          global.console[key] = jest.fn();
-        });
-      } // Argument is an object
-      else {
-          Object.keys(mockArg).forEach(function (key) {
-            global.console[key] = mockArg[key];
-          });
-        } // Return function to restore console
+    global.console[mockArg] = jest.fn();
+  } // Argument is an array
+  else if (Array.isArray(mockArg)) {
+    mockArg.forEach(function (key) {
+      global.console[key] = jest.fn();
+    });
+  } // Argument is an object
+  else {
+    Object.keys(mockArg).forEach(function (key) {
+      global.console[key] = mockArg[key];
+    });
+  } // Return function to restore console
 
-
-  var restore = function restore() {
-    return global.console = originalConsole;
-  };
 
   return function () {
     global.console = originalConsole;
